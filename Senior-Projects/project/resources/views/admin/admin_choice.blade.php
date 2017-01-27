0568965c444b70db9881f3343b8ac90003be6966
LanderVerschueren <verschueren@live.nl>
2017-01-27 16:39:53 +0100

    blablabla

--- Senior-Projects/project/resources/views/admin/admin_choice.blade.php
+++ Senior-Projects/project/resources/views/admin/admin_choice.blade.php
@@ -31,7 +31,7 @@
 				<a class="card_choice_link_admin" href="/keuze/{{ $choice->id }}">{{ $choice->choice }}</a>
 
 				@if($choice->elective->start_date < date("Y-m-d G:i:s"))
-				<a class="card_choice_edit modal-trigger" data-id="{{ $choice->id }}" data-toggle="modal" data-target="#editModal" data-title="{{ $choice->choice }}">
+				<a class="card_choice_edit modal-trigger" data-id="{{ $choice->id }}" data-toggle="modal" data-target="#editModal" data-title="{{ $choice->choice }}" data-test="{{ $choice->test_date }}" data-start="{{ $choice->start }}" data-end="{{ $choice->end }}">
 					<i class="fa fa-cog" aria-hidden="true"></i>
 				</a>
 				<a class="card_choice_delete modal-trigger" data-id="{{ $choice->id }}" data-toggle="modal" data-target="#deleteModal" data-title="{{ $choice->choice }}">
@@ -44,51 +44,36 @@
     </div>
 </content>
 
-<div class="modal fade" id="addCategoryModal" tabindex="-1" role="dialog" aria-labelledby="descriptionModalLabel">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title">Keuzevak toevoegen</h1>
-            </div>
-            <div class="modal-body">
-                <form method="POST" action="{{ url('/addElective') }}">
-                    {{ csrf_field() }}
-                    <div class="input-field">
-		                <input type="text" name="name">
-		                <label for="name">Naam</label>
-		            </div>
-		            <div class="input-field">
-		                <label for="start_date" class="active">Begindatum</label>
-		                <input class="form-control" type="date" name="start_date">
-		            </div>
-		            <div class="input-field">
-		                <label for="end_date" class="active">Einddatum</label>
-		                <input type="date" class="" name="end_date">
-		            </div>
-		            <button type="submit" class="button">Opslaan</button>
-                </form>
-            </div>
-            <div class="modal-footer">
-                
-            </div>
-        </div>
-    </div>
-</div>
-
 <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title" id="editModalLabel"></h1>
-            </div>
-            <div class="modal-body">
-                <p id="editModalParagraph"></p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="button" data-dismiss="modal">Sluiten</button>
-            </div>
-        </div>
-    </div>
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1 class="modal-title" id="editModalLabel"></h1>
+			</div>
+			<div class="modal-body">
+				<form method="POST" action="{{ url('/addElective') }}">
+					{{ csrf_field() }}
+					<div class="input-field">
+						<input type="text" name="name">
+						<label for="name">Naam</label>
+					</div>
+					<div class="input-field">
+						<label for="test_date" class="active">Proefdatum</label>
+						<input class="form-control" type="date" name="test_date" value="">
+					</div>
+					<div class="input-field">
+						<label for="start_date" class="active">Begindatum</label>
+						<input class="form-control" type="date" name="start_date">
+					</div>
+					<div class="input-field">
+						<label for="end_date" class="active">Einddatum</label>
+						<input type="date" class="" name="end_date">
+					</div>
+					<button type="submit" class="button">Opslaan</button>
+				</form>
+			</div>
+		</div>
+	</div>
 </div>
 
 <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel">
