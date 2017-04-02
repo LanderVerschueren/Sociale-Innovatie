e4e4cb1a5793519aafb328ac2412d74871de74f6
sepperenty <sepperenty@hotmail.com>
2017-04-02 21:17:46 +0200

    pagina om consultChoice toegevoegd

--- Senior-Projects/project/app/Http/Controllers/StudentController.php
+++ Senior-Projects/project/app/Http/Controllers/StudentController.php
@@ -114,7 +114,9 @@ class StudentController extends Controller
                 }
             }
         }
-        return $resultsForElective;
+       //return $resultsForElective;
+       debug($resultsForElective);
+        return view("pages.consultChoice", compact("resultsForElective", "elective"));
 
     }
 
